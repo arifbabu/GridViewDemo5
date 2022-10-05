ed426ac772a01167292c93109be5533e4f8be8b0
Arif Billah Babu <arif18bl@gmail.com>
2022-10-06 01:44:26 +0600

    GridViewDemo5 first Commit

---
+++ app/src/main/java/com/arifbabu/gridviewdemo5/MainActivity.java
@@ -0,0 +1,28 @@
+package com.arifbabu.gridviewdemo5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CursorAdapter;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+    GridView gridView;
+    int[] flags ={R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,
+            R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,R.drawable.ic_launcher_background};
+
+    String[] countryNames;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = findViewById(R.id.gridViewId);
+        countryNames = getResources().getStringArray(R.array.country_names);
+        CustomAdapter adapter = new CustomAdapter(this, flags, countryNames);
+        gridView.setAdapter(adapter);
+    }
+}
